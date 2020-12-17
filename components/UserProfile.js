0bfb846fb4f695a6ba26c793a38d1cc956d9772e
Yeongjae-Shin <syj9484@gmail.com>
2020-12-17 23:45:52 +0900

    Add: profile component

--- components/UserProfile.js
+++ components/UserProfile.js
@@ -1,16 +1,37 @@
-import React from "react";
-import Head from "next/head";
-import AppLayout from "../components/AppLayout";
+import React, { useCallback } from "react";
+import PropTypes from "prop-types";
+import { Card, Avatar, Button } from "antd";
 
-const UserProfile = () => {
+const UserProfile = ({ setIsLogin }) => {
+  const handleLogout = useCallback(() => {
+    setIsLogin(false);
+  }, []);
+
   return (
-    <>
-      <Head>
-        <title>Profile | NodeBird</title>
-      </Head>
-      <AppLayout>내 프로필</AppLayout>
-    </>
+    <Card
+      actions={[
+        <div key="twit">
+          쨱짹
+          <br />0
+        </div>,
+        <div key="following">
+          팔로잉
+          <br />0
+        </div>,
+        <div key="follower">
+          팔로워
+          <br />0
+        </div>,
+      ]}
+    >
+      <Card.Meta avatar={<Avatar>K</Avatar>} title="Konfusion"></Card.Meta>
+      <Button onClick={handleLogout}>로그아웃</Button>
+    </Card>
   );
+};
+
+UserProfile.propTypes = {
+  setIsLogin: PropTypes.func.isRequired,
 };
 
 export default UserProfile;
