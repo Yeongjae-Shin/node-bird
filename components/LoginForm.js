2a1dd0695bc7f544cc6cf2260f2c04ffa5bf4fd4
Yeongjae-Shin <syj9484@gmail.com>
2021-01-18 00:23:17 +0900

    [modify] states to custom hook

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -1,69 +1,59 @@
-import React, { useState, useCallback, useMemo } from "react";
-import Head from "next/head";
+import React, { useCallback, useMemo } from "react";
 import Link from "next/link";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 import { Form, Input, Button } from "antd";
+
+import useInput from "../lib/hooks/useInput";
 
 const FormStyle = styled(Form)`
   padding: 1rem;
 `;
 
 const LoginForm = ({ setIsLogin }) => {
-  const [values, setValues] = useState({});
-
-  const handleChange = useCallback(
-    ({ target: { name, value } }) => {
-      setValues({ ...values, [name]: value });
-    },
-    [values]
-  );
+  const [id, onChangeId] = useInput("");
+  const [password, onChangePassowrd] = useInput("");
 
   const handleSubmit = useCallback(() => {
     setIsLogin(true);
-  }, [values.id, values.password]);
+  }, [id, password]);
 
   const style = useMemo(() => ({ marginTop: 10 }), []);
 
   return (
-    <>
-      <Head>
-        <title>Login | NodeBird</title>
-      </Head>
-      <FormStyle onFinish={handleSubmit}>
-        <div>
-          <label htmlFor="id">아이디</label>
-          <Input
-            name="id"
-            value={values.id}
-            onChange={handleChange}
-            required
-            autoComplete="false"
-          />
-        </div>
-        <div>
-          <label htmlFor="password">비밀번호</label>
-          <Input
-            name="password"
-            type="password"
-            value={values.password}
-            onChange={handleChange}
-            required
-            autoComplete="false"
-          />
-        </div>
-        <div style={style}>
-          <Button type="primary" htmlType="submit" loading={false}>
-            로그인
-          </Button>
-          <Link href="/signup">
-            <a>
-              <Button>회원가입</Button>
-            </a>
-          </Link>
-        </div>
-      </FormStyle>
-    </>
+    <FormStyle onFinish={handleSubmit}>
+      <div>
+        <label htmlFor="id">아이디</label>
+        <Input
+          name="id"
+          value={id}
+          onChange={onChangeId}
+          required
+          autoComplete="false"
+        />
+      </div>
+      <div>
+        <label htmlFor="password">비밀번호</label>
+        <Input
+          name="password"
+          type="password"
+          value={password}
+          onChange={onChangePassowrd}
+          required
+          autoComplete="false"
+        />
+      </div>
+      <div style={style}>
+        <Button type="primary" htmlType="submit" loading={false}>
+          로그인
+        </Button>
+        <Link href="/signup">
+          <a>
+            <Button>회원가입</Button>
+          </a>
+        </Link>
+      </div>
+    </FormStyle>
   );
 };
 
